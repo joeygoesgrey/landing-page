84323ac98b289243bb28983ca817a252abd29a0a
joeygoesgrey <josephedomobi777@gmail.com>
2024-06-17 14:06:27 +0000

    first commit

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+get_header(); ?>
+
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/hero'); ?>
+<?php get_template_part('template-parts/results'); ?>
+<?php get_template_part('template-parts/testimonials'); ?>
+<?php get_template_part('template-parts/about'); ?>
+<?php get_template_part('template-parts/countdown-timer'); ?>
+ 
+
+<?php get_footer(); ?>
